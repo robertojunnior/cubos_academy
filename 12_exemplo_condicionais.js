339e7ceb0bfe9fa7b91b36084c69b724c5850792
roberjunnior <roberjunior.dev@gmail.com>
2023-06-16 12:46:13 -0300

    update

--- 12_exemplo_condicionais.js
+++ 12_exemplo_condicionais.js
@@ -1,5 +1,4 @@
 // Comprar apenas se o preço do produto 1 for igual 18.00 ou menos
-
 let preçoDoProduto1 = 17;
 let preçoDoProduto2 = 0.80;
 let preçoDoProduto3 = 4.85;
@@ -8,7 +7,6 @@ let custo = 0;
 if (preçoDoProduto1 <= 18.00) {
     console.log('Comprar produto.');
     custo = custo + preçoDoProduto1;
-    
 } else {
     console.log('Não comprar');
 }
@@ -17,7 +15,6 @@ if (preçoDoProduto2 < 3.00) {
     if (preçoDoProduto2 >= 1.50) {
     console.log('Comprar produto.');
     custo += preçoDoProduto2;
-
     } else {
         console.log('Não comprar, valor abaixo do mercado.');
 }
@@ -30,7 +27,6 @@ if (preçoDoProduto3 >= 4.85) {
     console.log('Não comprar.')
 }
 
-
-console.log('O custo total da compra foi de R$', custo.toFixed(2));
+console.log('O valor total da compra foi de R$', custo.toFixed(2));
 
 
