3c7ff02472722c06c4540560b97e33779b7a3298
roberjunnior <roberjunior.dev@gmail.com>
2023-06-19 00:05:43 -0300

    update

--- 18_elementos_condição.js
+++ 18_elementos_condição.js
@@ -1,11 +1,28 @@
-if (condição) {
+const palavra = 'paralelepipedo';
+let numeroDeVogais = 0;
 
+for (const letras of palavra) {
+    if (letras === 'a') {numeroDeVogais += 1;}
+    if (letras === 'e') {numeroDeVogais += 1;}
+    if (letras === 'i') {numeroDeVogais += 1;}
+    if (letras === 'o') {numeroDeVogais += 1;}
+    if (letras === 'u') {numeroDeVogais += 1;}
+    console.log(letras);
 }
 
-while (condição) {
 
-}
 
-for (const elemento of coleção) {
-    
-}
+
+
+
+// if (condição) {
+
+// }
+
+// while (condição) {
+
+// }
+
+// for (const elemento of coleção) {
+
+// }
