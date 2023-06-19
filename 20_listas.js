2096b71b4adbf456042be6ccb140f801bb410398
roberjunnior <roberjunior.dev@gmail.com>
2023-06-19 00:33:04 -0300

    update

---
+++ 20_listas.js
@@ -0,0 +1,8 @@
+//array são listas
+
+let lista = ['banana', 'maçã', 'açúcar', 'beterraba'];
+console.log(lista);
+// a const frutas, vai ser o elemento que o of do for, percorre dentro de uma Array(lista)
+for (const frutas of lista) { 
+    console.log(frutas);
+}
