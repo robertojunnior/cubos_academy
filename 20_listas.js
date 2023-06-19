7f9b3b10aa769104c890ec00b29749f7988d4251
roberjunnior <roberjunior.dev@gmail.com>
2023-06-19 00:51:30 -0300

    update ex 21 lsoma lista de numeros com for

--- 20_listas.js
+++ 20_listas.js
@@ -4,5 +4,6 @@ let lista = ['banana', 'maçã', 'açúcar', 'beterraba'];
 console.log(lista);
 // a const frutas, vai ser o elemento que o of do for, percorre dentro de uma Array(lista)
 for (const frutas of lista) { 
-    console.log(frutas);
+    console.log(frutas, typeof frutas);
+    
 }
---
+++ 21_ex_listas.js
@@ -0,0 +1,14 @@
+const listaDeNumeros = [34, 56, 768, 8, 810, 3, 6, 210];
+
+// 1. Qual a soma dos números da lista?
+let soma = 0;
+
+// para (cada elemento da lista)
+for (const numeros of listaDeNumeros) {
+    // some os elementos
+    soma += numeros; 
+}
+// mostre o resultado da soma dos elementos
+console.log(soma);
+
+// 2. Qual o menor número da lista?
