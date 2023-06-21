d4f820061b93cc712e2831c1004e7c1d37a350cf
roberjunnior <roberjunior.dev@gmail.com>
2023-06-20 23:56:40 -0300

    update

---
+++ 26_vencendo_o_jogo.js
@@ -0,0 +1,18 @@
+//Placar = [3, 2]
+//Se a quantidade de gols do TimeA for maior que a do TimeB, imprima TIME A;
+//Se não, se o Time B tiver mais gols, imprima TIME B;
+//E se o resultado estiver igual, imprima EMPATE.
+
+let placar = [3, 2]
+
+let golsTimeA = placar[0]
+let golsTimeB = placar[1]
+
+if (golsTimeA > golsTimeB){
+    console.log("TIME A")
+} else if (golsTimeA < golsTimeB){
+    console.log("TIME B")
+} else {
+    console.log("EMPATE")
+}
+
