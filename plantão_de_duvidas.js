d1b196a692b91f706f373549ebbbadac69ab086d
roberjunnior <roberjunior.dev@gmail.com>
2023-06-21 14:15:34 -0300

    update

---
+++ plantão_de_duvidas.js
@@ -0,0 +1,19 @@
+// entrada
+let notas = [5,10,10,8]
+let concluiuDesafios = false
+console.log('notas', notas)
+console.log('concluiu desafios?', concluiuDesafios)
+
+// programa
+let media = 0;
+
+
+if (concluiuDesafios === true) {
+   for (let nota of notas){
+      nota += nota + 1
+   }
+}
+
+
+//saida
+console.log(media)
