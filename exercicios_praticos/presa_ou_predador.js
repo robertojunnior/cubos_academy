6258e2662cd7343bcd6963d95fadadd582e54f86
roberjunnior <roberjunior.dev@gmail.com>
2023-06-22 03:04:27 -0300

    update exercicios

---
+++ exercicios_praticos/presa_ou_predador.js
@@ -0,0 +1,22 @@
+//Entrada
+// A entrada do seu programa é composta de uma variável do tipo booleana chamada olhosNasLaterais, que indica se o animal analisado tem ou não olhos nas laterais da cabeça. O valor de true quer dizer que o animal tem olhos nas laterais e false que não tem
+
+// Saída
+// Seu programa deve retornar:
+// PRESA: se o animal tiver olhos nas laterais da cabeça;
+// PREDADOR: se não tiver olhos nas laterais da cabeça.
+
+let olhosNasLaterais = true;
+
+if (olhosNasLaterais === true){
+    console.log("PRESA");
+} else {
+    console.log("PREDADOR")
+}
+
+//para a plataforma
+// if (olhosNasLaterais === true){
+//     return "PRESA";
+// } else {
+//     return "PREDADOR";
+// }
