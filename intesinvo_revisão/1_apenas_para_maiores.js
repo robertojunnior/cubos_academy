4efb807ea3f6a6bf4689f94b9c0f04b633db5fb7
roberjunnior <roberjunior.dev@gmail.com>
2023-06-22 00:48:29 -0300

    update

--- intesinvo_revisão/1_apenas_para_maiores.js
+++ intesinvo_revisão/1_apenas_para_maiores.js
@@ -1,4 +1,10 @@
 // se tiver 18 ou mais imprimir "PODE ENTRAR"
 // se tiver menos que 18 "ACESSO NEGADO"
 
-let idade 
+let idade = 17
+
+if (idade >= 18){
+    console.log("PODE ENTRAR")
+} else {
+    console.log("ACESSO NEGADO")
+}
