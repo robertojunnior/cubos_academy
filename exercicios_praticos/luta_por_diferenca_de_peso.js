f084a032b22b3fa29b44ae8d75f940a28f12fe0e
roberjunnior <roberjunior.dev@gmail.com>
2023-06-22 04:02:15 -0300

    update

---
+++ exercicios_praticos/luta_por_diferenca_de_peso.js
@@ -0,0 +1,26 @@
+let pesos = [74, 80];
+
+let diferenca = pesos[0] - pesos[1];
+if (diferenca < 0){
+    diferenca *= -1 // igual no curso do Gustavo guanabara
+}
+if (diferenca <= 5) {
+console.log("PODEM LUTAR");
+} else {
+console.log("NAO PODEM LUTAR");
+}
+
+// // para plataforma
+// let diferenca = pesos[0] - pesos[1];
+
+// if (diferenca < 0){
+//     diferenca *= -1 
+//     // caso o peso1 seja menor uso a multiplicação do negativo igual no excel e deixo sempre positivo
+// }
+// if (diferenca <= 5) {
+// return "PODEM LUTAR";
+// } else {
+// return "NAO PODEM LUTAR";
+// }
+
+
